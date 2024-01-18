55b6476967ef55d8f20d3906dde788038cc3b8b3
Shoaib <shoaibs411@gmail.com>
2024-01-19 01:21:34 +0530

    Creating Mutable App - TF AutoPush - Configuring mutable-app

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -2,10 +2,11 @@ module "cart"{
   source                  = "./vendor/modules/app"
 
   ENV                     = var.ENV
+  APP_VERSION             = var.APP_VERSION
   COMPONENT               = var.COMPONENT
   SPOT_INSTANCE_TYPE      = var.SPOT_INSTANCE_TYPE
   SPOT_INSTANCE_COUNT     = var.SPOT_INSTANCE_COUNT 
   OD_INSTANCE_TYPE        = var.OD_INSTANCE_TYPE
   OD_INSTANCE_COUNT       = var.OD_INSTANCE_COUNT
-
+  
 }
